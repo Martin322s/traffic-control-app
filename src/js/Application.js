05c78d3ac4814deead56565fa72f1c0444c8383c
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-12 15:29:05 +0200

    add bananas

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,21 +1,27 @@
 import EventEmitter from "eventemitter3";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.emojis = [];
-    this.banana = "🍌";
-    this.emit(Application.events.READY);
-  }
-  setEmojis(emojis) {
-    this.emojis = emojis;
-  }
+    constructor() {
+        super();
+        this.emojis = [];
+        this.banana = "🍌";
+        this.emit(Application.events.READY);
+    }
+    setEmojis(emojis) {
+        this.emojis = emojis;
+        const div = document.getElementById("emojis");
+        const p = document.createElement("p");
+        p.textContent = this.addBananas().join(" ");
+        div.appendChild(p);
+    }
 
-  addBananas() {}
+    addBananas() {
+        return this.emojis.map(x => x + this.banana);
+    }
 }
