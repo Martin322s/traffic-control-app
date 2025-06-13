65a607a2b3fff039c047ead543f4c17f7d54b883
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-06-13 15:59:27 +0300

    render everything in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import VideoStream from "./components/VideoStream";
+import React from "react";
+import VideoCanvas from "./components/VideoCanvas";
 
 function App() {
-
 	return (
-		<>
-			<VideoStream />
-		</>
-	)
+		<main>
+			<h2 style={{ textAlign: "center" }}>Видеонаблюдение – Кръстовище</h2>
+			<VideoCanvas />
+		</main>
+	);
 }
 
 export default App;
