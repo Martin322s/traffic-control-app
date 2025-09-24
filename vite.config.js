ef2a4cf98223c960e134352a8e60bcfb79c613de
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-24 15:42:14 +0300

    fix database problems

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		hmr: {
+			clientPort: 5173,
+		},
 		proxy: {
 			"/cam": {
 				target: "http://212.112.136.4:83",
